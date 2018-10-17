a1e6b73e9d0d132e4391848ee5913c12a13237e4
Robby Heenan <robbyheenan@Robbys-MacBook-Pro.local>
2018-10-16 20:27:41 -0400

    putting it together

--- client/src/components/Stattracker/Stattracker.js
+++ client/src/components/Stattracker/Stattracker.js
@@ -1,18 +1,35 @@
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
 import React from "react";
 import "./Stattracker.css";
 
 const Stattracker = props => (
-    <div class="col s10 m10">
-      <div class="card stattracker-tile">
+    <div className="col s10 m10">
+      <div className="card stattracker-tile">
         <div className="chart">
-        <LineChart width={700} height={200} data={props.data}>
+        <LineChart width={750} height={200} data={props.data}>
             <Line type="monotone" dataKey="distance" stroke="#8884d8" />
+            <XAxis/>
+            <YAxis/>
         </LineChart>
     </div>
+    <div className="divider"></div>
+  <div className="section">
+    <h5>Today</h5>
+    <p>numbers</p>
+  </div>
+  <div className="divider"></div>
+  <div className="section">
+    <h5>Week</h5>
+    <p>numbers</p>
+  </div>
+  <div className="divider"></div>
+  <div className="section">
+    <h5>Month</h5>
+    <p>numbers</p>
+  </div>
          
 
-      </div>
+    </div>
     </div>
 );
 
