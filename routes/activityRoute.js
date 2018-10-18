1f3344d0353f58a9d37709b6d255b17f67fdc792
sofyahoward <sofya.howard@gmail.com>
2018-10-18 19:29:02 -0400

    dry up code

--- routes/activityRoute.js
+++ routes/activityRoute.js
@@ -21,37 +21,39 @@ module.exports = app => {
         //   console.log(req._passport.session.user);
       });
 
-app.get('/api/activity/1w', (req, res)=>{
-    //distance for one month. If you want other endpoints-these are the options
-    // activities/calories
-    // activities/caloriesBMR
-    // activities/steps
-    // activities/distance
-    // activities/floors
-    // activities/elevation
-    // activities/minutesSedentary
-    // activities/minutesLightlyActive
-    // activities/minutesFairlyActive
-    // activities/minutesVeryActive
-    // activities/activityCalories
-    const URL = 'https://api.fitbit.com/1/user/-/activities/distance/date/today/1w.json';
-    request({
-        url: URL,
-        headers: {
-            'Authorization': `Bearer ${req.user.accessToken}`
-        }
-    }, (err, response, body) =>{
-        // console.log("Error", err);
-        // console.log("Response", response);
-        console.log("body", body)
-    } );
-    // console.log(req.user)
-      res.send(req.user);
-    //   console.log(req._passport.session.user);
-  });
-  app.get('/api/activity/1m', (req, res)=>{
+// app.get('/api/activity/1w', (req, res)=>{
+//     //distance for one month. If you want other endpoints-these are the options
+//     // activities/calories
+//     // activities/caloriesBMR
+//     // activities/steps
+//     // activities/distance
+//     // activities/floors
+//     // activities/elevation
+//     // activities/minutesSedentary
+//     // activities/minutesLightlyActive
+//     // activities/minutesFairlyActive
+//     // activities/minutesVeryActive
+//     // activities/activityCalories
+//     const URL = 'https://api.fitbit.com/1/user/-/activities/distance/date/today/1w.json';
+//     request({
+//         url: URL,
+//         headers: {
+//             'Authorization': `Bearer ${req.user.accessToken}`
+//         }
+//     }, (err, response, body) =>{
+//         // console.log("Error", err);
+//         // console.log("Response", response);
+//         console.log("body", body)
+//     } );
+//     // console.log(req.user)
+//       res.send(req.user);
+//     //   console.log(req._passport.session.user);
+//   });
+  app.get('/api/activity/:activityLength', (req, res)=>{
     //activity for 1 month
-    const URL = 'https://api.fitbit.com/1/user/-/activities/distance/date/today/1m.json';
+    if (/[1m|1w]/.test(req.params.activityLength)) {
+        const { activityLength } = req.params;
+        const URL = `https://api.fitbit.com/1/user/-/activities/distance/date/today/${activityLength}.json`;
     request({
         url: URL,
         headers: {
@@ -65,5 +67,9 @@ app.get('/api/activity/1w', (req, res)=>{
     // console.log(req.user)
       res.send(req.user);
     //   console.log(req._passport.session.user);
-  });
+ 
+    } else {
+        res.send("Route doesnt exist")
+    }
+});
 }
